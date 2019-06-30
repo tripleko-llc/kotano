cd0e01df9d8246ec48bcaeef6b1c96f7fbd8e1e9
Jared <jared.nishikawa@gmail.com>
2019-06-29 20:46:34 -0600

    method + context

--- kotano/__init__.py
+++ kotano/__init__.py
@@ -3,19 +3,21 @@ from functools import wraps
 import json
 
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'Tripleko LLC'
 __author_email__ = 'jared@tripleko.com'
 __description__ = 'Baby Hatano'
 
 
 class Request:
-    def __init__(self, params={}, data=b"", headers={}, multi={}, pathparams={}):
+    def __init__(self, params={}, data=b"", headers={}, multi={}, pathparams={}, method="", context={}):
         self.params = params
         self.data = data
         self.headers = headers
         self.multi = multi
         self.pathparams = pathparams
+        self.method = method
+        self.context = context
 
 
 class KotanoError(Exception):
@@ -39,12 +41,16 @@ def proxy(typ):
             headers = event.get('headers') or {}
             pathparams = event.get('pathParameters') or {}
             multi = event.get('multiValueQueryStringParameters') or {}
+            method = event.get("httpMethod") or ""
+            context = event.get("requestContext") or {}
             req = Request(
                     params=params,
                     data=data,
                     headers=headers,
                     multi=multi,
-                    pathparams=pathparams)
+                    pathparams=pathparams,
+                    method=method,
+                    context=context)
             body = inner(req)
             if not isinstance(body, str):
                 body = json.dumps(body)
